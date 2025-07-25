624e2b289ea2f896866822052ef65cfaab09f8bd
MartinBradburyPC <you@example.com>
2025-07-25 17:59:54 +0100

    updated login endpoint again

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -34,7 +34,7 @@ export default function Login() {
     setIsLoading(true);
     try {
       const response = await axios.post(
-        "https://exambuilder.herokuapp.com/accounts/login/",
+        "https://exambuilder-efae14d59f03.herokuapp.com/accounts/login/",
         formData
       );
 
