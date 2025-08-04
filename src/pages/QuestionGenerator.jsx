46312615636bf384ef1390564240d2ae061d9db2
Martin Bradbury <martinbradbury@btinternet.com>
2025-08-04 16:21:59 +0100

    Added questions additional question number to select

--- src/pages/QuestionGenerator.jsx
+++ src/pages/QuestionGenerator.jsx
@@ -218,9 +218,11 @@ export default function QuestionGenerator() {
               onChange={(e) => setNumberOfQuestions(e.target.value)}
               style={{ width: "100%", marginBottom: "1rem" }}
             >
-              <option value="1">1</option>
-              <option value="3">3</option>
-              <option value="5">5</option>
+              <option value="2">2</option>
+              <option value="4">4</option>
+              <option value="6">6</option>
+              <option value="8">8</option>
+              <option value="10">10</option>
             </select>
           </div>
           <button
